214720a2206aeb43e50c801f372c17dd250a0e59
CaMattos <camila.mattos11@outlook.com>
2017-02-22 17:00:25 -0300

    Meu projeto para estagio valemobi

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+
+mysql_connect('localhost','root','') or die("não conectado ao banco de dados");
+mysql_select_db('valemobi');
+
+?>
---
+++ contatos.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Camila Mattos">
+     <link rel="shortcut icon" type="image/x-icon" href="http://www.valemobi.com.br/favicon.ico"/>
+
+    <title>Teste online- ValeMobi 2017</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="bootstrap/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/dist/css/style.css" rel="stylesheet">
+
+    <script src="bootstrap/assets/js/ie-emulation-modes-warning.js"></script>
+
+    
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header clearfix">
+       <img src="_img/logo.png" class="img-responsive" alt="Responsive image" id="img-logo">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" ><a href="index.php">inicio</a></li>
+            <li role="presentation"  ><a href="cadastro.php">Cadastro</a></li>
+                  
+                  <li role="presentation" class="active" ><a href="contatos.php">Contatos</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Teste-Online</h3>
+      </div>
+
+      <div class="jumbotron">
+		
+		<img src="_img/img-contato.jpg" class="img-responsive" alt="Responsive image" id="img-contato">
+
+
+ 
+		<h1>
+		Camila J. Mattos
+		</h1>
+
+
+        <p class="lead">Estudante de Ciências da Computação - 6° semestre - UNIP</p>
+        <p>
+      
+		<div class="col-sm-12" id="info-contatos">
+		<p class="lead"><span class="glyphicon glyphicon-envelope" aria-hidden="true"> </span> &nbsp; camila.mattos11@outlook.com
+		</br>
+		<span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span>&nbsp; &nbsp;(11)98721-2324
+		</p
+		>
+		</div> <!-- end col-sm-1 -->
+
+		
+		
+
+
+            <a class="btn btn-lg btn-success" href="cadastro.php" role="button">Acessar GitHub</a></p>
+      </div>
+
+      
+
+      <footer class="footer">
+        <p>&copy; Teste online-2017 &nbsp; &nbsp; &nbsp;Desenvolvido por Camila Jenifer de Mattos</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
